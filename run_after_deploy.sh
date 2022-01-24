5f0a81e97eb35f46d9cdf8c2915cf7bbe8bd966f
Reuben Cleetus <marlinspike@users.noreply.github.com>
2022-01-23 19:45:32 -0500

    Delete run_after_deploy.sh

--- run_after_deploy.sh
+++
@@ -1,5 +0,0 @@
-export KV_NAME=${1:-$(terraform output kv_name)}
-source ../scripts/fetch-kubeconfig.sh
-echo $KUBECONFIG
-az keyvault secret show --name node-key --vault-name $KV_NAME | jq -r '.value' > rke2.priv_key
-chmod 400 rke2.priv_key
